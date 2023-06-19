4f8cf8db1c34d81d12a7c1a868bdd16a8aa85f94
riigee <gerard.marti.gallego@estudiantat.upc.edu>
2023-06-19 17:19:42 +0200

    Counter fixed

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,23 @@
 
 import React, { useState } from "react";
+import './Counter.css';
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    
+    const doDonation = (event) => {
+        event.Default();
+      };
+
     return (
-        <div>
-            <button onClick={() => setCount((x) => x-1)}>-</button>
+        <div className="donations">
+            <button onClick={() => setCount((x) => Math.max(x - 1, 0))}>-</button>
             <span className="counter" style={{fontSize: "200%", padding:"0 2em"}}>{count}</span>
             <button onClick={() => setCount((x) => x+1)}>+</button>
-        </div>
+      <form onSubmit={doDonation}>
+        <button>Donate</button>
+      </form>
+    </div>
+        
     );
 }
