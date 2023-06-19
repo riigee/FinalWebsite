6b3a1fe9cf202d81cc2eaf5994e9dfa09d24fb75
riigee <gerard.marti.gallego@estudiantat.upc.edu>
2023-06-19 20:26:21 +0200

    Final news and early social media idea

--- src/components/ColumnRight.jsx
+++ src/components/ColumnRight.jsx
@@ -1,10 +1,13 @@
 import './ColumnRight.css';
 
-function ColumnRight ({ title, description}) {
+function ColumnRight ({ title, description, title1, description1}) {
     return(
         <div className="column-right">
             <h2>{title}</h2>
             <p>{description}</p>
+            
+            <h2>{title1}</h2>
+            <p>{description1}</p>
         </div>
     );
 }
