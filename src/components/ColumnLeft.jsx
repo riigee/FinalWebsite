9fea2cedd9423a7cafe99eaa1bce64426fe7274f
riigee <gerard.marti.gallego@estudiantat.upc.edu>
2023-06-19 16:11:53 +0200

    Main things on left column

--- src/components/ColumnLeft.jsx
+++ src/components/ColumnLeft.jsx
@@ -1,14 +1,14 @@
 
-import './ColumnSite.css';
+import './ColumnLeft.css';
 
 function ColumnSite ({ title, description, date}) {
     return(
         <div className="column-site">
             <h2>{title}</h2>
-            <p>{description}</p>
             <div class="top">
                 <time>{date}</time>
             </div>
+            <p>{description}</p>
         </div>
     );
 }
